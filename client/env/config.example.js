ce142860d1a8330bcb200d169c36d31815055e12
Jerome Rodriguez <jeromemtrodriguez@gmail.com>
2022-06-10 15:20:42 -0700

    added token and campus

--- client/env/config.example.js
+++ client/env/config.example.js
@@ -3,8 +3,8 @@
 
 // Put your parse application keys here!
 $.ajaxPrefilter(function (settings, _, jqXHR) {
-  jqXHR.setRequestHeader('Authorization', 'GITHUB_API_TOKEN');
+  jqXHR.setRequestHeader('Authorization', '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');
 });
 
 // Put your campus prefix here
-window.CAMPUS = 'FILL_ME_IN';
+window.CAMPUS = 'rfp';
